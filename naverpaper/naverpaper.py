cd7d780971910cb925ee4853fb20c54e5d8205e4
jungx098 <jungx098@gmail.com>
2023-12-23 05:29:10 -0600

    UA Update for iOS Safari

--- naverpaper/naverpaper.py
+++ naverpaper/naverpaper.py
@@ -41,7 +41,7 @@ def naver_session(nid, npw):
     )
     s.mount('https://', HTTPAdapter(max_retries=retries))
     request_headers = {
-        'User-agent': 'Mozilla/5.0'
+        'User-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 17_1_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1 Mobile/15E148 Safari/604.1'
     }
 
     bvsd_uuid = uuid.uuid4()
