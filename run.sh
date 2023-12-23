6890a788180565dac83605763d5458a806ddf156
jungx098 <jungx098@gmail.com>
2023-12-23 07:19:47 -0600

    Shell Wrappe

---
+++ run.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env sh
+
+# Platform specific commands
+SHUF=""
+NOSLEEP=""
+
+# TODO: [ is not posix compatible, and failed on raspberry. Fix condition
+#       statement.
+if [ "$(uname)" == "Darwin" ]; then
+    # Mac OS X platform
+    SHUF="/opt/local/bin/gshuf"
+    NOSLEEP="pmset noidle"
+    :
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # GNU/Linux platform
+    SHUF="shuf"
+    :
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Windows NT platform
+    SHUF="shuf"
+    :
+elif [ "$(expr substr $(uname -s) 1 9)" == "CYGWIN_NT" ]; then
+    # Cygwin NT platform
+    SHUF="shuf"
+    NOSLEEP="/opt/local/bin/nosleep.sh"
+fi
+
+# Random sleep duration in seconds between 0 and 1800 (30 mins)
+DURATION=$($SHUF -i 0-1800 -n 1)
+
+if [ -n "$1" ]; then
+    DURATION="$1"
+fi
+
+# Exit if network is not available
+nc -zw1 google.com 443 || \
+   { echo "$(basename $0) No network connection: $(date)"; exit; }
+
+# Start time stamp
+echo "$(basename $0) Start: $(date)"
+
+# Do not go to sleep.
+if [ -n "$NOSLEEP" ]; then
+    sh -c "$NOSLEEP" &
+    NOSLEEP_PID=$!
+fi
+
+# Obtain current working directory and script directory
+OLD_PATH=$(pwd)
+SCRIPT_PATH=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+
+# Change to script directory
+cd $SCRIPT_PATH
+
+# Run main script after random delay seconds.
+sleep $DURATION
+./main.py
+
+# Change back to old working directory
+cd $OLD_PATH
+
+# Okay to go to sleep.
+if [ -n "$NOSLEEP" ]; then
+    kill $NOSLEEP_PID $(pgrep -P $NOSLEEP_PID)
+fi
+
+# End time stamp
+echo "$(basename $0) End: $(date)"
