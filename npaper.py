0e661f81cc2420a3dbc038786e6a954d915ee080
jungx098 <jungx098@gmail.com>
2024-07-02 21:25:56 -0500

    Logging Verbose Levels

--- npaper.py
+++ npaper.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import argparse
 import json
 import logging
@@ -12,6 +14,7 @@ from run_new import visit
 
 
 class CustomFormatter(logging.Formatter):
+    """Custom formatter for logging."""
 
     grey = "\x1b[38;20m"
     blue = "\x1b[38;5;39m"
@@ -19,14 +22,16 @@ class CustomFormatter(logging.Formatter):
     red = "\x1b[31;20m"
     bold_red = "\x1b[31;1m"
     reset = "\x1b[0m"
-    log = "%(asctime)s [%(levelname)s] %(message)s (%(filename)s:%(lineno)d)"
+
+    format_simple = "%(asctime)s [%(levelname)s] %(message)s"
+    format_detail = "%(asctime)s [%(levelname)s] %(message)s (%(filename)s:%(lineno)d)"
 
     FORMATS = {
-        logging.DEBUG: grey + log + reset,
-        logging.INFO: blue + log + reset,
-        logging.WARNING: yellow + log + reset,
-        logging.ERROR: red + log + reset,
-        logging.CRITICAL: bold_red + log + reset
+        logging.DEBUG: grey + format_simple + reset,
+        logging.INFO: blue + format_simple + reset,
+        logging.WARNING: yellow + format_simple + reset,
+        logging.ERROR: red + format_detail + reset,
+        logging.CRITICAL: bold_red + format_detail + reset,
     }
 
     def format(self, record):
@@ -35,14 +40,28 @@ class CustomFormatter(logging.Formatter):
         return formatter.format(record)
 
 
-def init_logger(debug: bool = True):
+def init_logger(verbose: int = 0):
     """Function initializing logger."""
+
+    LEVEL = {
+        5: logging.DEBUG,
+        4: logging.INFO,
+        3: logging.WARNING,
+        2: logging.ERROR,
+        1: logging.CRITICAL,
+    }
+
+    level = logging.CRITICAL + 1
+    if verbose > 5:
+        level = logging.DEBUG
+    elif verbose > 0:
+        level = LEVEL[verbose]
 
     ch = logging.StreamHandler()
     ch.setFormatter(CustomFormatter())
 
     logging.basicConfig(
-        level=logging.DEBUG if debug is True else logging.CRITICAL + 1,
+        level=level,
         handlers=[ch],
     )
 
@@ -53,9 +72,8 @@ def get_balance(driver):
 
     try:
         driver.get("https://new-m.pay.naver.com/mydata/home")
-        balance = driver.find_element(By.CLASS_NAME,
-                                      "AssetCommonItem_balance__mkiEz")
-        balance = int(re.sub(r'[^0-9]', '', balance.text))
+        balance = driver.find_element(By.CLASS_NAME, "AssetCommonItem_balance__mkiEz")
+        balance = int(re.sub(r"[^0-9]", "", balance.text))
     except Exception as e:
         logging.warning("%s: Balance Not Available!", e)
 
@@ -71,53 +89,73 @@ def main(campaign_links, id, pwd, ua, headless, newsave):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--id', type=str, required=False, help="naver id")
-    parser.add_argument('-p', '--pw', type=str, required=False, help="naver password")
-    parser.add_argument('-c', '--cd', type=str, required=False, help="credential json")
-    parser.add_argument('--headless', type=bool, required=False,
-                        default=True, action=argparse.BooleanOptionalAction,
-                        help="browser headless mode (default: headless)")
-    parser.add_argument('--newsave', type=bool, required=False,
-                        default=False, action=argparse.BooleanOptionalAction,
-                        help="new save or do not")
-    parser.add_argument('-cf', '--credential-file', type=str, required=False,
-                        help="credential json file")
-    parser.add_argument('-d', '--debug', type=bool, required=False,
-                        default=False, action=argparse.BooleanOptionalAction,
-                        help="debug mode")
+    parser.add_argument("-i", "--id", type=str, required=False, help="naver id")
+    parser.add_argument("-p", "--pw", type=str, required=False, help="naver password")
+    parser.add_argument("-c", "--cd", type=str, required=False, help="credential json")
+    parser.add_argument(
+        "--headless",
+        type=bool,
+        required=False,
+        default=True,
+        action=argparse.BooleanOptionalAction,
+        help="browser headless mode (default: headless)",
+    )
+    parser.add_argument(
+        "--newsave",
+        type=bool,
+        required=False,
+        default=False,
+        action=argparse.BooleanOptionalAction,
+        help="new save or do not",
+    )
+    parser.add_argument(
+        "-cf",
+        "--credential-file",
+        type=str,
+        required=False,
+        help="credential json file",
+    )
+    parser.add_argument("-v", "--verbose", action="count", default=0)
+
     args = parser.parse_args()
     cd_obj = None
     headless = args.headless
     newsave = args.newsave
-    init_logger(args.debug)
 
-    if (args.id is None and
-            args.pw is None and
-            args.cd is None and
-            args.credential_file is None):
+    init_logger(args.verbose)
+
+    if (
+        args.id is None
+        and args.pw is None
+        and args.cd is None
+        and args.credential_file is None
+    ):
         id = os.getenv("USERNAME")
         pw = os.getenv("PASSWORD")
-        if(pw is None and pw is None):
-            print('not setting USERNAME / PASSWORD')
+        if pw is None and pw is None:
+            print("not setting USERNAME / PASSWORD")
             exit()
         cd_obj = [{"id": id, "pw": pw}]
-    elif(args.cd is not None):
+    elif args.cd is not None:
         try:
             cd_obj = json.loads(args.cd)
-        except:
-            print('use -c or --cd argument')
-            print('credential json sample [{"id":"id1","pw":"pw1"},{"id":"id2","pw":"pw2"}]')
-            print('json generate site https://jsoneditoronline.org/')
+        except Exception as e:
+            logging.critical("%s: json loading error", e)
+            print("use -c or --cd argument")
+            print(
+                'credential json sample [{"id":"id1","pw":"pw1"},{"id":"id2","pw":"pw2"}]'
+            )
+            print("json generate site https://jsoneditoronline.org/")
             exit()
     elif args.credential_file is not None:
         file_obj = open(args.credential_file, "r", encoding="utf-8")
         cd_obj = json.load(file_obj)
     else:
         if args.id is None:
-            print('use -i or --id argument')
+            print("use -i or --id argument")
             exit()
         if args.pw is None:
-            print('use -p or --pwd argument')
+            print("use -p or --pwd argument")
             exit()
         cd_obj = [{"id": args.id, "pw": args.pw}]
 
